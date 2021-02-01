e25afbc33d0ceb4707a358d69d32f7fe65d5b1fa
taiga <taiga.similarities@gmail.com>
2021-02-01 16:48:48 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about Next.js features and API..</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
